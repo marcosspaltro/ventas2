dde9260492e01b1bd0889fa0e6187f04bf069047
marcos spaltro <msspaltro@yahoo.com>
2020-07-01 22:21:16 -0300

    Initial commit

---
+++ ventas2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ventas2
+{
+    partial class ventas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstProductos = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.txtProductos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstProductos
+            // 
+            this.lstProductos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstProductos.FormattingEnabled = true;
+            this.lstProductos.Location = new System.Drawing.Point(12, 14);
+            this.lstProductos.Name = "lstProductos";
+            this.lstProductos.Size = new System.Drawing.Size(776, 394);
+            this.lstProductos.TabIndex = 1;
+            this.lstProductos.SelectedIndexChanged += new System.EventHandler(this.lstProductos_SelectedIndexChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAgregar.Location = new System.Drawing.Point(217, 413);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "&Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnEditar.Location = new System.Drawing.Point(301, 413);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "&Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBorrar.Location = new System.Drawing.Point(382, 413);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "&Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // txtProductos
+            // 
+            this.txtProductos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtProductos.Location = new System.Drawing.Point(12, 415);
+            this.txtProductos.Name = "txtProductos";
+            this.txtProductos.Size = new System.Drawing.Size(199, 20);
+            this.txtProductos.TabIndex = 5;
+            // 
+            // ventas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtProductos);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstProductos);
+            this.IsMdiContainer = true;
+            this.Name = "ventas";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ventas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProductos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TextBox txtProductos;
+    }
+}
+
---
+++ ventas2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ventas2
+{
+    public partial class ventas : Form
+    {
+        public ventas()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ventas_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("Productos.txt"))
+            {
+                StreamWriter swProductos = new StreamWriter("Productos.txt");
+                swProductos.WriteLine("");
+                swProductos.Close();
+            }
+            else
+            {
+                TextReader lector = new StreamReader("Productos.txt");
+                string[] leer = File.ReadAllLines("Productos.txt");
+                foreach (string item in leer)
+                {
+                    lstProductos.Items.Add(item);
+                }
+                lector.Close();
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtProductos.Text != "")
+                lstProductos.Items.Add(txtProductos.Text);
+                txtProductos.Text = "";
+            nvalista();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (lstProductos.SelectedIndex != -1)
+                lstProductos.Items.RemoveAt(lstProductos.SelectedIndex);
+            txtProductos.Text = "";
+            nvalista();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+                if (txtProductos.Text != "" & lstProductos.SelectedIndex != -1)
+                {
+                    lstProductos.Items.Insert(lstProductos.SelectedIndex, txtProductos.Text);
+                    lstProductos.Items.RemoveAt(lstProductos.SelectedIndex);
+                    btnEditar.Text = "Editar";
+                    txtProductos.Text = "";
+                    nvalista();
+
+                }
+                else
+                {
+                    btnEditar.Text = "Aceptar";
+                    txtProductos.Text = lstProductos.SelectedItem.ToString();
+                }
+               
+        }
+
+        private void lstProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void nvalista()
+        {
+            TextWriter nvoarchivo = new StreamWriter("Productos.txt");
+            foreach (object lista in lstProductos.Items)
+                nvoarchivo.WriteLine(lista);
+            nvoarchivo.Close();
+        }
+    }
+}
